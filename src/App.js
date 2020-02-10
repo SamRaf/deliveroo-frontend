4719bfe6037404238783e5cf8a63e00c7d84b11c
SamRaf <samrafrafi93@gmail.com>
2020-02-10 15:45:29 +0100

    http

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://sam-deliveroo-backend.herokuapp.com/"
+      );
+      setData(response.data);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+
+  return (
+    <div className="App">
+      {isLoading === true ? (
+        <p>En cours de chargement ...</p>
+      ) : (
+        <div>{data.restaurant.name}</div>
+      )}
+    </div>
+  );
+}
+
+export default App;
